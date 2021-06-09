9b80195bbef4d47031cb164381a5ae4c0b2b3fe3
kouta0530 <16jk047@ymklab.net>
2021-06-09 14:03:57 +0900

    feat(polls): detailに404エラーの処理を追加

--- polls/views.py
+++ polls/views.py
@@ -13,7 +13,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You`re looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoseNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
