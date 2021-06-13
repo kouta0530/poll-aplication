56deb78e1435f8da9677b472a72d1c519acfb63c
kouta0530 <16jk047@ymklab.net>
2021-06-13 12:20:02 +0900

    fix(polls): importのtypoを修正

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-from djnago.utils import timezone
+from django.utils import timezone
 
 from .models import Question, Choice
 # Create your views here.
