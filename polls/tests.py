de660d4650db24ff331d272916fd675ffd9d5f6f
kouta0530 <16jk047@ymklab.net>
2021-06-13 12:33:35 +0900

    feat(polls): 過去の質問と未来の質問が投稿された時、過去の質問だけが表示されているかテストする

--- polls/tests.py
+++ polls/tests.py
@@ -45,3 +45,10 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             [question],
         )
+
+    def test_future_question_and_past_question(self):
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'],
+                                 [question])
