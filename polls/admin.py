fc6c23a35365c07ae800e4c9eaed1152e724652a
kouta0530 <16jk047@ymklab.net>
2021-06-27 22:52:48 +0900

    feat(polls): Questionモデルのquestion_textに検索バーを追加

--- polls/admin.py
+++ polls/admin.py
@@ -17,6 +17,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
+    search_fields = ['question_text']
     inlines = [ChoiceInline]
 
 
